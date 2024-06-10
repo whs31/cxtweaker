f2f1dd2aecffca9efd62b6282ce8375b9b559a8d
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-06-10 19:07:39 +0300

    Better AST dump

---
+++ src/algo/misc.rs
@@ -0,0 +1,21 @@
+use colored::Colorize;
+use crate::pb_print;
+
+pub fn ast_dump(entity: &clang::Entity) -> ()
+{
+  pb_print!("  [{:^20}] {:<50} in file <{}>",
+    format!("{:?}", entity.get_kind()).bold().yellow(),
+    entity.get_name().unwrap_or("<unknown>".to_string()).bold().green(),
+    match entity.get_location() {
+      Some(loc) => {
+        let loc = loc.get_file_location();
+        let file_str = match loc.file {
+          Some(file) => format!("{}", file.get_path().file_name().unwrap().to_os_string().into_string().unwrap()),
+          None => "unknown".to_string()
+        };
+        format!("{}:{}:{}", file_str.bold().magenta(), loc.line.to_string().italic(), loc.column.to_string().italic())
+      },
+      None => "unknown".to_string().bold().magenta().to_string()
+    }
+  );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::rc::Rc;
 use clap::Parser;
-use colored::Colorize;
 use crate::core::args;
 
 mod core;
 mod parser;
+mod algo;
 
 fn try_main() -> anyhow::Result<()>
 {
@@ -16,24 +16,7 @@ fn try_main() -> anyhow::Result<()>
   match &args.command {
     Some(args::Command::Process(argv)) => {
       let parser = parser::Parser::new(argv, false)?;
-      parser.parse(argv, |entity| {
-        pb_print!("  [{}] {} in <{}>",
-          format!("{:?}", entity.get_kind()).bold().yellow(),
-          entity.get_name().unwrap_or("<unknown>".to_string()).bold().green(),
-          match entity.get_location() {
-            Some(loc) => {
-              let loc = loc.get_file_location();
-              let file_str = match loc.file {
-                Some(file) => format!("{}", file.get_path().file_name().unwrap().to_os_string().into_string().unwrap()),
-                None => "unknown".to_string()
-              };
-              format!("{}:{}:{}", file_str.bold().magenta(), loc.line.to_string().italic(), loc.column.to_string().italic())
-            },
-            None => "unknown".to_string().bold().magenta().to_string()
-          }
-        );
-        //println!("{:?}", entity.get_name());
-      })?;
+      parser.parse(argv, algo::misc::ast_dump)?;
     }
     None => {
       return Err(anyhow::anyhow!("no command specified. see --help"));
