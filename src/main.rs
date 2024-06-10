d1e08840aef0ecc13804924707b598b9acccfebc
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-06-10 18:57:55 +0300

    AST dump

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 use clap::Parser;
+use colored::Colorize;
 use crate::core::args;
 
 mod core;
@@ -16,6 +17,21 @@ fn try_main() -> anyhow::Result<()>
     Some(args::Command::Process(argv)) => {
       let parser = parser::Parser::new(argv, false)?;
       parser.parse(argv, |entity| {
+        pb_print!("  [{}] {} in <{}>",
+          format!("{:?}", entity.get_kind()).bold().yellow(),
+          entity.get_name().unwrap_or("<unknown>".to_string()).bold().green(),
+          match entity.get_location() {
+            Some(loc) => {
+              let loc = loc.get_file_location();
+              let file_str = match loc.file {
+                Some(file) => format!("{}", file.get_path().file_name().unwrap().to_os_string().into_string().unwrap()),
+                None => "unknown".to_string()
+              };
+              format!("{}:{}:{}", file_str.bold().magenta(), loc.line.to_string().italic(), loc.column.to_string().italic())
+            },
+            None => "unknown".to_string().bold().magenta().to_string()
+          }
+        );
         //println!("{:?}", entity.get_name());
       })?;
     }
